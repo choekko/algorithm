93a30a527430b3dee1fcc5ad3401ba5a0d19888e
choekko <choekko@gmail.com>
2023-03-27 00:49:02 +0900

    [programmers] '3 X N 타일링' 문제 시간초과 해결

--- Javascript/3xN타일링.js
+++ Javascript/3xN타일링.js
@@ -1,13 +1,13 @@
 // https://school.programmers.co.kr/learn/courses/30/lessons/12902
 
 function solution(n) {
-  const dp = [0n, 0n, 3n, 0n, 11n];
-  const NUMBER_OF_IRREPLACABLE_PER_WIDTH = 2n;
+  const dp = [0, 0, 3, 0, 11];
+  const NUMBER_OF_IRREPLACABLE_PER_WIDTH = 2;
   // 4이상 짝수 width 별로 약수 width 들의 배치 조합으로는 대체할 수 없는 배치가 2개씩 있다.
 
   for (let i = 5; i <= n; i++) {
     if (i % 2) {
-      dp.push(0n);
+      dp.push(0);
       continue;
     }
     let value = NUMBER_OF_IRREPLACABLE_PER_WIDTH;
@@ -19,8 +19,8 @@ function solution(n) {
       }
       value += dp[j] * NUMBER_OF_IRREPLACABLE_PER_WIDTH;
     }
-    dp.push(value);
+    dp.push(value % 1000000007);
   }
 
-  return dp[n] % 1000000007n
+  return dp[n];
 }
